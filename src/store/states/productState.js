909a220cba7be6ed50449a98a5ce298a1a8088f0
Mukonyole Evans <emukonyole@gmail.com>
2025-01-07 14:16:57 +0000

    images and app states and slices

---
+++ src/store/states/productState.js
@@ -0,0 +1,103 @@
+const productState = [
+  {
+    id: "laptop1",
+    title: "HP Elitebook 650 G10 15.6",
+    price: 1200,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/61j92LBE9KL._AC_SX425_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "laptop2",
+    title: "ASUS : 2023 Vivobook 17X Laptop",
+    price: 1000,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71gLz5gSDjL._AC_UY218_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "laptop3",
+    title: "HP Elitebook 660 G11 16",
+    price: 1250,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71EPfXnafGL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "laptop4",
+    title: "Lenovo ThinkBook 16 G6 16",
+    price: 1150,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/81dDVDtwlvL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "printer1",
+    title: "HP OfficeJet Pro 9135e All-in-One Printer",
+    price: 400,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/61ynr8Smk+L._AC_SY300_SX300_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "printer2",
+    title: "HP 58X Black High-yield Toner Cartridge",
+    price: 300,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/711-hEL7EcL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "card1",
+    title: "SanDisk 2TB Extreme microSDXC UHS-I Memory Card with Adapter",
+    price: 250,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71ENEUtLFtL._AC_SX425_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "laptop5",
+    title: "Apple MacBook Air 13.6 with Liquid Retina Display",
+    price: 1600,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/611zv2cWnTL._AC_UY218_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "laptop6",
+    title: "Oemgenuine 2024 Dell Latitude 3550 Notebook 15.6” FHD",
+    price: 1000,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71GPPXZsaML.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "laptop7",
+    title: "Innoview 23.8” 4K Portable Monitor",
+    price: 370,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/718hjyQT6dL._AC_SX425_.jpg",
+    quantity: 0,
+    added: false,
+  },
+  {
+    id: "laptop8",
+    title: "Laptop Screen Extender, 14 FHD 1080P Laptop Monitor Extender",
+    price: 270,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/71t6wn0UyGL._AC_SX425_.jpg",
+    quantity: 0,
+    added: false,
+  },
+];
+
+export { productState };

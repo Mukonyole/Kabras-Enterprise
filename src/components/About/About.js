7535f9c42e50d5d88c337927ee6bf4b352d4045c
Evans Mukonyole <emukonyole@gmail.com>
2025-01-07 16:47:53 +0300

    Create About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="banner-image-about">
+        <div className="about-kabras">
+          <h2 className="about-kabras-text">OUR MISSION</h2>
+          <p>
+            "Empowering Your Digital World with Reliable Technology Solutions"
+          </p>
+        </div>
+      </div>
+      <div className="about-kabras-part">
+        <h2 className="about-kabras-heading">About Kabras Enterprise</h2>
+        <p className='about-kabras-para'>
+        At Kabras Enterprise, we are passionate about providing high-quality computers,
+        accessories, and technology solutions to meet the diverse needs of our customers. 
+        Founded with the belief that technology should empower individuals and businesses, 
+        we are dedicated to offering the best products and services to help you stay ahead in the digital world.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
